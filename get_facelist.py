aeb76f0bc38678e01d432c2875a8e863c63ac6e3
infinityslayer52 <35188111+infinityslayer52@users.noreply.github.com>
2018-02-20 10:49:35 +0530

    Update get_facelist.py

--- get_facelist.py
+++ get_facelist.py
@@ -1,23 +1,36 @@
 import requests
 import pprint
 
-#initializing required variables
-faceListId='class_1'
-c=0
-
-params_get={
-    'faceListId':faceListId
+# initializing required variables
+params_get = {
+    'faceListId': ''
 }
 
-headers_get={
-    'Ocp-Apim-Subscription-Key':'1a35269c87e5479ab6ed638399b5a449'
+headers_get = {
+    'Ocp-Apim-Subscription-Key': '1a35269c87e5479ab6ed638399b5a449'
 }
 
-#api call to display list of faces in specified list
-r=requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}',params=params_get,headers=headers_get)
-print('Faces present in this list are- ')
-for face in range(0,len(r.json()['persistedFaces'])):
-    pprint.pprint(r.json()['persistedFaces'][face]['userData'])
-    c+=1
+headers_getlist = {
+    'Ocp-Apim-Subscription-Key': '1a35269c87e5479ab6ed638399b5a449'
+}
 
-print('Total number of faces is ',c)
+
+def listed():
+    c = 0
+    s = requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists', headers=headers_getlist)
+    if len(s.json()) == 0:
+        print('No groups found.')
+    else:
+        print('The groups are as follows-')
+        for lists in range(0, len(s.json())):
+            pprint.pprint(s.json()[lists]['faceListId'])
+        groupName = input('Enter the name of the group whose students you wish to see.')
+        params_get['faceListId'] = groupName
+        # api call to display list of faces in specified list
+        r = requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}', params=params_get, headers=headers_get)
+        print('Students present in this group are- ')
+        for face in range(0, len(r.json()['persistedFaces'])):
+            pprint.pprint(r.json()['persistedFaces'][face]['userData'])
+            # pprint.pprint(r.json()['persistedFaces'][face]['persistedFaceId'])
+            c += 1
+        print('Total number of students is ', c)
