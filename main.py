742a05402b583af95cca7a2b4a5e1fd1048b885a
infinityslayer52 <35188111+infinityslayer52@users.noreply.github.com>
2018-02-20 10:46:47 +0530

    main.py added
    Main UX created. Only call main.py to control complete code.

---
+++ main.py
@@ -0,0 +1,42 @@
+import create_facelist
+import delete_facelist
+import get_facelist
+import add_facestolist
+import delete_face
+import search_infacelists
+import add_facetolist
+
+print('1. Create a group.')
+print('2. Add multiple students to a group from a folder.')
+print('3. Add a single student from a file.')
+print('4. Display list of students in a group.')
+print('5. Mark attendance for a group from a photo.')
+print('6. Delete a student from a group.')
+print('7. Delete a group.')
+n = True
+while n:
+    x = input('Choose a valid option')
+    if x == '1':
+        create_facelist.create()
+    elif x == '2':
+        add_facestolist.add_faces()
+    elif x == '3':
+        add_facetolist.add()
+    elif x == '4':
+        get_facelist.listed()
+    elif x == '5':
+        search_infacelists.search()
+    elif x == '6':
+        delete_face.delete()
+    elif x == '7':
+        delete_facelist.delete()
+    else:
+        print('Please enter a valid option.')
+    r = input('Do you want to perform another task?(y/n)')
+    if r == 'n' or r == 'N':
+        n = False
+        print('Exiting...')
+    elif r == 'y' or r == 'Y':
+        continue
+    else:
+        print('Exiting...')
