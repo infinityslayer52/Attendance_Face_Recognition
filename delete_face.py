6084614768537d4ea647b3764fa13fa10e9f4c77
infinityslayer52 <35188111+infinityslayer52@users.noreply.github.com>
2018-02-20 10:48:39 +0530

    Update delete_face.py

--- delete_face.py
+++ delete_face.py
@@ -1,35 +1,55 @@
 import requests
 import pprint
 
-faceListId='class_1'
-faceId='4'
-params_delete={
-    'faceListId':faceListId,
-    'persistedFaceId':''
+params_delete = {
+    'faceListId': '',
+    'persistedFaceId': ''
 }
 
-headers_delete={
-    'Ocp-Apim-Subscription-Key':'1a35269c87e5479ab6ed638399b5a449'
+headers_delete = {
+    'Ocp-Apim-Subscription-Key': '1a35269c87e5479ab6ed638399b5a449'
 }
 
-params_get={
-    'faceListId':faceListId
+params_get = {
+    'faceListId': ''
 }
 
-headers_get={
-    'Ocp-Apim-Subscription-Key':'1a35269c87e5479ab6ed638399b5a449'
+headers_get = {
+    'Ocp-Apim-Subscription-Key': '1a35269c87e5479ab6ed638399b5a449'
 }
 
-r=requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}',params=params_get,headers=headers_get)
-#pprint.pprint(r)
-for face in range(0,len(r.json()['persistedFaces'])):
-    if(faceId==r.json()['persistedFaces'][face]['userData']):
-        params_delete['persistedFaceId']=r.json()['persistedFaces'][face]['persistedFaceId']
-        s=requests.delete(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}/persistedFaces/{persistedFaceId}',params=params_delete,headers=headers_delete)
-        #pprint.pprint(s)
-        if(int(r.status_code)==200):
-            print(faceId,' Face Deleted')
+headers_getlist = {
+    'Ocp-Apim-Subscription-Key': '1a35269c87e5479ab6ed638399b5a449'
+}
+
+
+def delete():
+    t = requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists', headers=headers_getlist)
+    if len(t.json()) == 0:
+        print('No groups found.')
+    else:
+        print('The groups are as follows-')
+        for lists in range(0, len(t.json())):
+            pprint.pprint(t.json()[lists]['faceListId'])
+        groupName = input('Enter the name of the group from which a student has to be deleted')
+        params_delete['faceListId'] = groupName
+        params_get['faceListId'] = groupName
+        r = requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}', params=params_get, headers=headers_get)
+        # pprint.pprint(r)
+        if len(r.json()['persistedFaces']) == 0:
+            print('No students present in this group.')
         else:
-            print(faceId,' Face not Deleted. Error-',(r.status_code))
-        break
-
+            print('The students present in this group are- ')
+            for face in range(0, len(r.json()['persistedFaces'])):
+                pprint.pprint(r.json()['persistedFaces'][face]['userData'])
+            faceId = input('Enter the name of the student who has to be deleted from this list')
+            for face in range(0,len(r.json()['persistedFaces'])):
+                if faceId == r.json()['persistedFaces'][face]['userData']:
+                    params_delete['persistedFaceId'] = r.json()['persistedFaces'][face]['persistedFaceId']
+                    s = requests.delete(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}/persistedFaces/{persistedFaceId}', params=params_delete, headers=headers_delete)
+                    # pprint.pprint(s)
+                    if int(s.status_code) == 200:
+                        print(faceId, ' was deleted from the group ', groupName)
+                    else:
+                        print(faceId, ' was not Deleted. Error-', r.status_code)
+                    break
