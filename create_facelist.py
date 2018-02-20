4db7efa6152cfe2046bdf8727d249e8de75d8521
infinityslayer52 <35188111+infinityslayer52@users.noreply.github.com>
2018-02-20 10:48:10 +0530

    Update create_facelist.py

--- create_facelist.py
+++ create_facelist.py
@@ -1,28 +1,27 @@
 import requests
 import pprint
 
-#initializing required variables
-name='class_1'
-userData='Class 1'
-faceListId='class_1'
-
-params_create={
-    'faceListId':faceListId
+params_create = {
+    'faceListId': ''
 }
 
-headers_create={
-    'Ocp-Apim-Subscription-Key':'1a35269c87e5479ab6ed638399b5a449'
+headers_create = {
+    'Ocp-Apim-Subscription-Key': '1a35269c87e5479ab6ed638399b5a449'
 }
 
-body_create={
-    'name':name,
-    'userData':userData
+body_create = {
+    'name': ''
 }
 
-#api call to create face list
-r=requests.put(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}',json=body_create,headers=headers_create,params=params_create)
-#pprint.pprint(r.status_code)
-if(int(r.status_code)==200):
-    print('Face List Created')
-else:
-    print('Face List not Created. Error-',(r.status_code))
+
+def create():
+    print('Enter the name of the group')
+    groupName = input('Note- Name of the group should be in small letters and should not contain spaces. Hyphens and underscores are allowed.')
+    params_create['faceListId'] = groupName
+    body_create['name'] = groupName
+    r = requests.put(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}', json=body_create, headers=headers_create, params=params_create)
+    # pprint.pprint(r.status_code)
+    if int(r.status_code) == 200:
+        print('Group named ', groupName, ' created.')
+    else:
+        print('Group not Created. Error-', r.status_code)
