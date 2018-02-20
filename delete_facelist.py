a8c32cdc578294483c7a0a148417797e64ab6427
infinityslayer52 <35188111+infinityslayer52@users.noreply.github.com>
2018-02-20 10:49:14 +0530

    Update delete_facelist.py

--- delete_facelist.py
+++ delete_facelist.py
@@ -1,19 +1,33 @@
 import requests
 import pprint
 
-#initializing required variables
-params_delete={
-    'faceListId':'class_1'
+# initializing required variables
+params_delete = {
+    'faceListId': ''
 }
 
-headers_delete={
-    'Ocp-Apim-Subscription-Key':'1a35269c87e5479ab6ed638399b5a449'
+headers_delete = {
+    'Ocp-Apim-Subscription-Key': '1a35269c87e5479ab6ed638399b5a449'
 }
 
-#api call to delete face list
-r=requests.delete(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}',headers=headers_delete,params=params_delete)
-#pprint.pprint(r.status_code)
-if(int(r.status_code)==200):
-    print('Face List Deleted')
-else:
-    print('Face List not Deleted. Error-',(r.status_code))
+headers_getlist = {
+    'Ocp-Apim-Subscription-Key': '1a35269c87e5479ab6ed638399b5a449'
+}
+
+
+def delete():
+    s = requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists', headers=headers_getlist)
+    if len(s.json()) == 0:
+        print('No groups found.')
+    else:
+        print('List of groups are as follows- ')
+        for list in range(0, len(s.json())):
+            pprint.pprint(s.json()[list]['faceListId'])
+        groupName = input('Enter the name of the group you want to delete.')
+        params_delete['faceListId'] = groupName
+        r = requests.delete(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}', headers=headers_delete, params=params_delete)
+        # pprint.pprint(r.status_code)
+        if int(r.status_code) == 200:
+            print('Group named ', groupName, ' deleted')
+        else:
+            print('Group not deleted. Error-', r.status_code)
