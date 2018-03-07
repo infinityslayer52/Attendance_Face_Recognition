c611f90ee4e0372ce0ff87bb64a9c3b281bcc4fc
infinityslayer52 <35188111+infinityslayer52@users.noreply.github.com>
2018-03-07 11:58:04 +0530

    Update delete_facelist.py

--- delete_facelist.py
+++ delete_facelist.py
@@ -1,7 +1,6 @@
 import requests
 import pprint
 
-# initializing required variables
 params_delete = {
     'faceListId': ''
 }
@@ -21,13 +20,24 @@ def delete():
         print('No groups found.')
     else:
         print('List of groups are as follows- ')
-        for list in range(0, len(s.json())):
-            pprint.pprint(s.json()[list]['faceListId'])
-        groupName = input('Enter the name of the group you want to delete.')
-        params_delete['faceListId'] = groupName
-        r = requests.delete(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}', headers=headers_delete, params=params_delete)
-        # pprint.pprint(r.status_code)
-        if int(r.status_code) == 200:
-            print('Group named ', groupName, ' deleted')
+        for lists in range(0, len(s.json())):
+            pprint.pprint(s.json()[lists]['faceListId'])
+        groupname = input('Enter the name of the group you want to delete.')
+        x = 0
+        for i in range(0, len(s.json())):
+            if groupname == s.json()[i]['faceListId']:
+                x = 1
+                break
+            else:
+                continue
+        if x == 1:
+            params_delete['faceListId'] = groupname
+            r = requests.delete(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}', headers=headers_delete, params=params_delete)
+            # pprint.pprint(r.status_code)
+            if int(r.status_code) == 200:
+                print('Group named ', groupname, ' deleted')
+            else:
+                print('Group not deleted. Error-', r.status_code)
         else:
-            print('Group not deleted. Error-', r.status_code)
+            print("The group name that you entered was not found. PLease enter a valid group name.")
+            delete()
