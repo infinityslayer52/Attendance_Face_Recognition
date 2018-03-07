b078603beb0b345b20bd933315e32c970ef9811a
infinityslayer52 <35188111+infinityslayer52@users.noreply.github.com>
2018-03-07 11:59:30 +0530

    Update search_infacelists.py

--- search_infacelists.py
+++ search_infacelists.py
@@ -1,9 +1,6 @@
 import requests
 import pprint
 
-# initializing required variables
-# img='./samples/sample1.jpg'
-
 headers_search = {
     'Ocp-Apim-Subscription-Key': '1a35269c87e5479ab6ed638399b5a449'
 }
@@ -37,29 +34,43 @@ def search():
     u = requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists', headers=headers_getlist)
     for lists in range(0, len(u.json())):
         pprint.pprint(u.json()[lists]['faceListId'])
-    groupName = input('Enter the name of the group for which attendance has to be marked.')
-    body_search['faceListId'] = groupName
-    params_get['faceListId'] = groupName
-    img = input('Enter the location of the image taken during attendance.')
-    # searching and mapping found faces
-    t = requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}', params=params_get, headers=headers_get)
-    # pprint.pprint(t.json()['persistedFaces'])
-    img = open(img, 'rb').read()
-    r = requests.post(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect', headers=headers_detect, data=img)
-    # pprint.pprint(r.json())
-    for face in range(0, len(r.json())):
-        body_search['faceId'] = r.json()[face]['faceId']
-        s = requests.post(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/findsimilars', headers=headers_search, json=body_search)
-        # pprint.pprint(s.json())
-        if s.json() == []:
+    groupname = input('Enter the name of the group for which attendance has to be marked.')
+    x = 0
+    for i in range(0, len(u.json())):
+        if groupname == u.json()[i]['faceListId']:
+            x = 1
+            break
+        else:
             continue
-        elif s.json()[0]['confidence'] > 0.85:
-            f = s.json()[0]['persistedFaceId']
-            for faces in range(0, len(t.json()['persistedFaces'])):
-                if f == t.json()['persistedFaces'][faces]['persistedFaceId']:
-                    found.append(t.json()['persistedFaces'][faces]['userData'])
-                    break
-
-    found.sort(key=int)
-    print('The student who were present in the class were-')
-    print(found)
+    if x == 1:
+        body_search['faceListId'] = groupname
+        params_get['faceListId'] = groupname
+        img = input('Enter the location of the image taken during attendance.')
+        # searching and mapping found faces
+        t = requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}', params=params_get, headers=headers_get)
+        # pprint.pprint(t.json()['persistedFaces'])
+        img = open(img, 'rb').read()
+        r = requests.post(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect', headers=headers_detect, data=img)
+        # pprint.pprint(r.json())
+        for face in range(0, len(r.json())):
+            body_search['faceId'] = r.json()[face]['faceId']
+            s = requests.post(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/findsimilars', headers=headers_search, json=body_search)
+            pprint.pprint(s.json())
+            if not s.json():
+                continue
+            elif s.json()[0]['confidence'] > 0.5:
+                f = s.json()[0]['persistedFaceId']
+                for faces in range(0, len(t.json()['persistedFaces'])):
+                    if f == t.json()['persistedFaces'][faces]['persistedFaceId']:
+                        found.append(t.json()['persistedFaces'][faces]['userData'])
+                        break
+        print('The students who were present in the class were-')
+        '''
+        r = sorted(found, key=lambda item: (int(item.partition(' ')[0])
+                                           if item[0].isdigit() else float('inf'), item))
+        print(', '.join(r))
+        '''
+        print(found)
+    else:
+        print("The group name that you entered was not found. PLease enter a valid group name.")
+        search()
