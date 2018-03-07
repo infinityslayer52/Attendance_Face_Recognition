ec6ce15edbdd89352037da928631885f25fdcace
infinityslayer52 <35188111+infinityslayer52@users.noreply.github.com>
2018-03-07 11:58:26 +0530

    Update get_facelist.py

--- get_facelist.py
+++ get_facelist.py
@@ -1,7 +1,6 @@
 import requests
 import pprint
 
-# initializing required variables
 params_get = {
     'faceListId': ''
 }
@@ -24,13 +23,23 @@ def listed():
         print('The groups are as follows-')
         for lists in range(0, len(s.json())):
             pprint.pprint(s.json()[lists]['faceListId'])
-        groupName = input('Enter the name of the group whose students you wish to see.')
-        params_get['faceListId'] = groupName
-        # api call to display list of faces in specified list
-        r = requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}', params=params_get, headers=headers_get)
-        print('Students present in this group are- ')
-        for face in range(0, len(r.json()['persistedFaces'])):
-            pprint.pprint(r.json()['persistedFaces'][face]['userData'])
-            # pprint.pprint(r.json()['persistedFaces'][face]['persistedFaceId'])
-            c += 1
-        print('Total number of students is ', c)
+        groupname = input('Enter the name of the group whose students you wish to see.')
+        x = 0
+        for i in range(0, len(s.json())):
+            if groupname == s.json()[i]['faceListId']:
+                x = 1
+                break
+            else:
+                continue
+        if x == 1:
+            params_get['faceListId'] = groupname
+            r = requests.get(url='https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/{faceListId}', params=params_get, headers=headers_get)
+            print('Students present in this group are- ')
+            for face in range(0, len(r.json()['persistedFaces'])):
+                pprint.pprint(r.json()['persistedFaces'][face]['userData'])
+                # pprint.pprint(r.json()['persistedFaces'][face]['persistedFaceId'])
+                c += 1
+            print('Total number of students is ', c)
+        else:
+            print("The group name that you entered was not found. PLease enter a valid group name.")
+            listed()
